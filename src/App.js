3cd8af91c4f793b3b366981e63fbb1e7b94cb5fa
Ajaybabu21 <ajaybabukanjirakkattu2@gmail.com>
2023-10-31 16:11:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const Calculator = () => {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleButtonClick = (value) => {
+    setInput(input + value);
+  };
+
+  const calculateResult = () => {
+    try {
+      setResult(eval(input).toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  };
+
+  const clearInput = () => {
+    setInput("");
+    setResult("");
+  };
+
+  return (
+    
+    <div className="calculator">
+      <h1>Calculator</h1>
+      <div className="display">
+        <input type="text" value={input} readOnly />
+        <div className="result">{result}</div>
+      </div>
+      <div className="buttons">
+        <div className="row">
+          <button onClick={() => handleButtonClick("7")}>7</button>
+          <button onClick={() => handleButtonClick("8")}>8</button>
+          <button onClick={() => handleButtonClick("9")}>9</button>
+          <button onClick={() => handleButtonClick("/")}>/</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick("4")}>4</button>
+          <button onClick={() => handleButtonClick("5")}>5</button>
+          <button onClick={() => handleButtonClick("6")}>6</button>
+          <button onClick={() => handleButtonClick("*")}>*</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick("1")}>1</button>
+          <button onClick={() => handleButtonClick("2")}>2</button>
+          <button onClick={() => handleButtonClick("3")}>3</button>
+          <button onClick={() => handleButtonClick("-")}>-</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick("0")}>0</button>
+          <button onClick={() => handleButtonClick(".")}>.</button>
+          <button onClick={calculateResult}>=</button>
+          <button onClick={() => handleButtonClick("+")}>+</button>
+        </div>
+        <div className="row">
+          <button className="clear" onClick={clearInput}>
+            C
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calculator />
     </div>
   );
 }
